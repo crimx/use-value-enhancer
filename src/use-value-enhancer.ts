ec29eb2176a87d2b65e790d0de6a4c42d9e83cad
CRIMX <straybugs@gmail.com>
2022-09-02 13:59:54 +0800

    refactor: upgrade to value-enhancer 2.0

--- src/use-value-enhancer.ts
+++ src/use-value-enhancer.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import type { ReadonlyVal } from "value-enhancer";
 
-export function useVal<TValue = any, TMeta = any>(
-  val: ReadonlyVal<TValue, TMeta>
+export function useVal<TValue = any>(
+  val: ReadonlyVal<TValue>,
+  eager = false
 ): TValue {
   const [value, setValue] = useState(val.value);
-  useEffect(() => val.subscribe(setValue), [val]);
+  useEffect(() => val.subscribe(setValue, eager), [val]);
   return value;
 }
 
