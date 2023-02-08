a05551fb0b6a4e1e35c1dcebcb31e484967251c8
crimx <straybugs@gmail.com>
2023-02-08 16:47:40 +0800

    refactor: support custom val compare

--- src/use-value-enhancer.ts
+++ src/use-value-enhancer.ts
@@ -1,12 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import type { ReadonlyVal } from "value-enhancer";
 
 export function useVal<TValue = any>(
   val: ReadonlyVal<TValue>,
   eager = false
 ): TValue {
-  const [value, setValue] = useState(() => val.value);
-  useEffect(() => val.subscribe(value => setValue(() => value), eager), [val]);
+  // val could have custom compare function, so we need to use a state to trigger re-rendering
+  const [updateId, updater] = useState(0);
+  // Only access value getter when updateId changes to avoid frequent getter calls
+  const value = useMemo(() => val.value, [updateId]);
+  useEffect(() => {
+    let isFirst = true;
+    return val.subscribe(newValue => {
+      if (isFirst) {
+        isFirst = false;
+        // useEffect is called asynchronously so we need to check again
+        if (val.compare(newValue, value)) {
+          return;
+        }
+      }
+      updater(updateId => (updateId + 1) % 4294967296);
+    }, eager);
+  }, [val]);
   return value;
 }
 
--- test/use-value-enhancer.test.ts
+++ test/use-value-enhancer.test.ts
@@ -39,4 +39,38 @@ describe("useVal", () => {
   it("should export useValueEnhancer alias", () => {
     expect(useValueEnhancer).toBe(useVal);
   });
+
+  it("should not trigger extra rendering on initial value", async () => {
+    const val$ = val(1);
+    let renderingCount = 0;
+    const { result } = renderHook(() => {
+      renderingCount += 1;
+      return useVal(val$);
+    });
+
+    expect(result.current).toBe(1);
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+
+    expect(renderingCount).toBe(1);
+  });
+
+  it("should not trigger extra rendering on same value", async () => {
+    const val$ = val({ a: 1 }, { compare: (a, b) => a.a === b.a });
+    let renderingCount = 0;
+    const { result } = renderHook(() => {
+      renderingCount += 1;
+      return useVal(val$);
+    });
+
+    await act(async () => val$.set({ a: 1 }));
+    await act(async () => val$.set({ a: 1 }));
+    await act(async () => val$.set({ a: 1 }));
+
+    expect(result.current).toEqual({ a: 1 });
+
+    await new Promise(resolve => setTimeout(resolve, 0));
+
+    expect(renderingCount).toBe(1);
+  });
 });
